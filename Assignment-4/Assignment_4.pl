37e980db77df6e25f03fa877e6b3906c0dfdee56
Brandon Rossi <brandonrossisjsu@gmail.com>
2014-03-11 14:13:48 -0700

    Start of program
    this is the start of the program

--- Assignment-4/Assignment_4.pl
+++ Assignment-4/Assignment_4.pl
@@ -1,1 +1,31 @@
-person(bob).
+% == CS 152 Section 05 Group: The Phantoms ==
+% ======= Assignment 4: Program.pl ========
+% By Christopher Raleigh and Brandon Rossi 
+ 
+person(professor_plum).
+person(colonel_mustard).
+person(miss_scarrlet).
+person(ms_peacock).
+person(mr_green).
+person(miss_white).
+person(dr_black).
+
+weapon(candlestick).
+weapon(pipe).
+weapon(revolver).
+weapon(spanner).
+weapon(knife).
+
+location(dining_room, knife).
+location(study, revolver).
+location(kitchen, knife).
+location(lounge, candlestick).
+location(hall, candlestick).
+location(ballroom, candlestick).
+location(conservatory, pipe).
+location(cellar, spanner).
+location(library, candlestick).
+location(billiard_room, revolver).
+
+killed(dr_black, candlestick, ballroom).
+
