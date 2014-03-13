d76eea993ffd714b8a92aedf0c56f804472607fc
Christopher Raleigh <craleigh318@comcast.net>
2014-03-13 16:02:58 -0700

    Combined some rules.
    Right now, we have almost enough rules, but we need to make sure that
    the queries lead us to exactly one suspect.

--- Assignment-4/Assignment_4.pl
+++ Assignment-4/Assignment_4.pl
@@ -66,6 +66,7 @@ likes(mrs_white, cooking).
 %Rules
 married(Suspect1, Suspect2) :- shares_hobby(Suspect1, Suspect2, Hobby1),
 	shares_hobby(Suspect1, Suspect2, Hobby2),
+	Hobby1 \= Hobby2,
 	Suspect1 \= miss_scarrlet,
 	Suspect2 \= miss_scarrlet,
 	Suspect1 \= Suspect2,
@@ -78,25 +79,27 @@ shares_hobby(Suspect1, Suspect2) :- likes(Suspect1, Hobby1),
 shares_hobby(Suspect1, Suspect2, Hobby) :- likes(Suspect1, Hobby),
 	likes(Suspect2, Hobby),
 	Suspect1 \= Suspect2.
-opposite_sex(Suspect1, Suspect2) :- male(Suspect1),
-	female(Suspect2).
 
-opposite_sex(Suspect1, Suspect2) :- male(Suspect2),
-	female(Suspect1).
+opposite_sex(Suspect1, Suspect2) :- ((male(Suspect1),
+				      female(Suspect2));
+				    (male(Suspect2),
+				     female(Suspect1))).
 
 preferred_weapon(Suspect1, revolver) :- person(Suspect1, colonel).
 preferred_weapon(Suspect1, knife) :- person(Suspect1, cook).
-preferred_weapon(Suspect1, candlestick) :- likes(Suspect1, dancing).
-preferred_weapon(Suspect1, candlestick) :- likes(Suspect1, relaxing).
+preferred_weapon(Suspect1, candlestick) :- (likes(Suspect1, dancing);
+					   likes(Suspect1, relaxing)).
 preferred_weapon(Suspect1, pipe) :- is_strong(Suspect1).
-preferred_weapon(Suspect1, spanner) :- person(Suspect1, botanist).
-preferred_weapon(Suspect1, spanner) :- preferred_weapon(Suspect1, pipe).
+preferred_weapon(Suspect1, spanner) :- (person(Suspect1, botanist);
+				       preferred_weapon(Suspect1, pipe)).
 
 person(Suspect1, cook) :- person(Suspect1, maid).
-person(Suspect1, spanner) :- person(Suspect1, land_scaper).
+preferred_weapon(Suspect1, spanner) :- person(Suspect1, land_scaper).
 
 is_strong(Suspect1) :- male(Suspect1).
 
 is_visitor(Suspect1) :- \+ is_resident(Suspect1).
 
 is_suspect(Suspect1) :- Suspect1 \= dr_black.
+
+likes(Suspect1, cleaning) :- person(Suspect1, maid).
