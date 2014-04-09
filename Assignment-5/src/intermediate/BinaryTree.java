6c70c45ed553b9654a89e3661a959d98c4c0cac9
Brandon Rossi <BrandonRossi@Brandons-MacBook-Pro-4.local>
2014-04-09 12:48:47 -0700

    Small Changes to BTree Code
    Working on the preorder traverse of the BTree.

--- Assignment-5/src/intermediate/BinaryTree.java
+++ Assignment-5/src/intermediate/BinaryTree.java
@@ -1,7 +1,9 @@
 package intermediate;
 
+import javax.sound.midi.SysexMessage;
+
 /**
- *
+ * BinaryTree is the data structure that holds the code after if has been parsed
  * @author BrandonRossi
  */
 public class BinaryTree
@@ -17,15 +19,16 @@ public class BinaryTree
      * Adding a new element to the tree
      * @param element the element to be added to the tree
      */
-    public void addNode(String element)
+    public void addNode(String element, boolean isElement)
     {
-        Node newNode = new Node(element);
+        Node newNode = new Node(element, isElement);
         //If the root is null then the new element is the first element in the tree
         if(root == null)
         {
             root = newNode;
         }
-        else//Otherwise add the new node to the tree
+        //Otherwise add the new node to the tree
+        else
         {
             Node currentNode = root;
             Node parent;
@@ -34,20 +37,19 @@ public class BinaryTree
             {
                 parent = currentNode;
                 //Set the left child
-                if(/*Some how have to determine how to set the child based on a key or the '(' and ')'*/)
+                if(/*NOT SURE IF THIS IS RIGHT*/currentNode.leftChild == null)/*Some how have to determine how to set the child based on a key or the '(' and ')'*/
                 {
                     currentNode = currentNode.leftChild;
-
                     if(currentNode == null)
                     {
                         parent.leftChild = newNode;
-                        return
+                        return;
                     }
                 }
-                else//set the right child
+                //set the right child
+                else
                 {
                     currentNode = currentNode.rightChild;
-
                     if(currentNode == null)
                     {
                         parent.rightChild = newNode;
@@ -58,14 +60,43 @@ public class BinaryTree
         }
     }
 
+    public void preOrderTraverse(Node currentNode)
+    {
+        if(currentNode != null)
+        {
+            /*
+             * Need to have code for if the left child is not an element then open
+             * '(' otherwise print the element and dont forget to close the ')'
+             * Visit the root.
+             *      If the left subtree is not an element node, open a set of parentheses.
+             * Visit the left subtree.
+             *      If the left subtree is a leaf, print its element.
+             * Visit the right subtree.
+             */
+            if(!currentNode.leftChild.isElement)
+            {
+                System.out.print("(");
+                if(currentNode.leftChild == null)
+                {
+                    System.out.println(currentNode.element);
+                }
+                preOrderTraverse(currentNode.leftChild);
+                preOrderTraverse(currentNode.rightChild);
+                System.out.println(")");
+            }
+        }
+    }
+
     private class Node
     {
         String element;
+        boolean isElement;
         Node leftChild;
         Node rightChild;
 
-        public Node(String str)
+        public Node(String str, boolean isElement)
         {
+            this.isElement = isElement;
             element = str;
         }
 
