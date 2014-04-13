f40d83c95f07a50bc437d4023f479523e85afe88
Brandon Rossi <BrandonRossi@Brandons-MacBook-Pro-4.local>
2014-04-12 20:40:17 -0700

    Simple working Scanner
    This scanner splits the file almost perfect. just have some formatting
    issues

--- Assignment-5/src/backend/Executor.java
+++ Assignment-5/src/backend/Executor.java
@@ -3,7 +3,6 @@
  * and open the template in the editor.
  */
 package backend;
-import frontend.SchemeScanner;
 import frontend.SchemeScanner2;
 import frontend.Token;
 import java.io.File;
@@ -19,25 +18,26 @@ public class Executor
         try
         {
             String file = "";
-            File inputFile = new File("input 3.txt");
+            File inputFile = new File("input.txt");
             Scanner fileScanner = new Scanner(inputFile);
 
             //Reads in the whole file into a single string
             while(fileScanner.hasNextLine())
             {
-                file += fileScanner.nextLine();
+                file += fileScanner.nextLine().replaceAll(";.*", " ");
             }
-            //Removes all comments from the file
-            file = file.replaceAll(";.*", "");
-            //Splits the file up into lines and places each line in an array
-            String[] splitList = file.split("\n");
+
+            file = file.replaceAll("\\(", "\\( ");
+            file = file.replaceAll("\\)", "\\ )");
+            file = file.replaceAll("\n", "");
+
             //Passes the Array of lines to the SchemeScanner
-            SchemeScanner2 scanner = new SchemeScanner2(splitList);
+            SchemeScanner2 scanner = new SchemeScanner2(file);
 
             //Testing the scanner output
             Token t = scanner.nextToken();
             System.out.println(t.getName());
-            while(!t.getType().equals("END_OF_INPUT") || !t.getType().equals("ERROR"))
+            while(!t.getType().equals("END_OF_INPUT") && !t.getType().equals("ERROR"))
             {
                 t = scanner.nextToken();
                 System.out.println(t.getName());
--- Assignment-5/src/frontend/SchemeScanner2.java
+++ Assignment-5/src/frontend/SchemeScanner2.java
@@ -5,6 +5,8 @@
 package frontend;
 
 import java.util.HashMap;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 /**
  *
@@ -17,25 +19,18 @@ public class SchemeScanner2
     private final String identifier = "IDENTIFIER";
     private final String special_Symbol = "SPECIAL_SYMBOL";
     private final String procedure = "PROCEDURE";
-    //The array of strings each index contains a line of code from the file
-    private String[] splitList;
-    //The index into the array of lines
-    private int splitListIndex;
-    //This is the index of the string. traversing over the line is done with this index
-    private int lineIndex;
+
+    private String file;
+    Scanner fileScanner;
     HashMap<String, String> key_Word_Map;
     HashMap<String, String> special_Symbol_Map;
     HashMap<String, String> procedure_Symbol_Map;
 
-    public SchemeScanner2(String[] split)
+    public SchemeScanner2(String file)
     {
-        //This is the array
-        splitList = split;
-        //This is the array index
-        splitListIndex = 0;
-        //This is the string index
-        lineIndex = 0;
-
+        //This is the file
+        this.file = file;
+        fileScanner = new Scanner(this.file);
         key_Word_Map = new HashMap<>();
         special_Symbol_Map = new HashMap<>();
         procedure_Symbol_Map = new HashMap<>();
@@ -45,243 +40,14 @@ public class SchemeScanner2
 
     public Token nextToken()
     {
-        //A new Token
-        Token currentToken;
-        //As a token is read from the string it is placed into this string
-        String token = "";
-        //The current line that is being read from
-        String currentLine = splitList[splitListIndex];
-
-        //if the index into the string is greater then the length of the string go to a new line
-        if (lineIndex >= currentLine.length())
+        try
         {
-            lineIndex = 0;
-            splitListIndex += 1;
-            if (splitListIndex >= splitList.length)
-            {
-                return new Token("END OF INPUT", Token.Type.END_OF_INPUT);
-            }
-            currentLine = splitList[splitListIndex];
+            String currentToken = fileScanner.next();
+            return new Token(currentToken, Token.Type.IDENTIFIER);
+        } catch (NoSuchElementException e)
+        {
+            return new Token("ERROR", Token.Type.ERROR);
         }
-        //if the index into the array of lines is greater then the length then end of file
-        if (splitListIndex >= splitList.length)
-        {
-            return new Token("END OF INPUT", Token.Type.END_OF_INPUT);
-        }
-        //If the current line is empty then move to the next line
-        while (currentLine.equals(""))
-        {
-            if (splitListIndex < splitList.length)
-            {
-                splitListIndex += 1;
-                currentLine = splitList[splitListIndex];
-                lineIndex = 0;
-            }
-        }
-        //If the current character is a space then incrament the index by 1 to go to the next character
-        while (currentLine.charAt(lineIndex) == ' ')
-        {
-            lineIndex++;
-            if (lineIndex >= currentLine.length())
-            {
-                splitListIndex++;
-                if (splitListIndex >= splitList.length)
-                {
-                    return new Token("END OF INPUT", Token.Type.END_OF_INPUT);
-                }
-                currentLine = splitList[splitListIndex];
-                lineIndex = 0;
-
-            }
-        }
-
-        //If the current character is a ( create a new token and return it
-        //incrament the index into the string
-        if (currentLine.charAt(lineIndex) == '(')
-        {
-            //currentToken = new Token("(", Token.Type.SPECIAL_SYMBOL);
-            lineIndex++;
-            return new Token("(", Token.Type.SPECIAL_SYMBOL);
-        } //If the current chara ter is a ) then create a new token and return it
-        //incrament the index into the string
-        else if (currentLine.charAt(lineIndex) == ')')
-        {
-            //currentToken = new Token(")", Token.Type.SPECIAL_SYMBOL);
-            lineIndex++;
-            return new Token(")", Token.Type.SPECIAL_SYMBOL);
-        } //Check for procedures
-        else if (is_Procedure(Character.toString(currentLine.charAt(lineIndex))))
-        {
-            Token temp = new Token(Character.toString(currentLine.charAt(lineIndex)), Token.Type.PROCEDURE);
-            lineIndex++;
-            if (lineIndex >= currentLine.length())
-            {
-                splitListIndex++;
-                if (splitListIndex >= splitList.length)
-                {
-                    return new Token("END OF INPUT", Token.Type.END_OF_INPUT);
-                }
-                currentLine = splitList[splitListIndex];
-                lineIndex = 0;
-
-            }
-            return temp;
-        } //Checks for special Symbols excluding ( and )
-        else if (is_Special_Symbol(Character.toString(currentLine.charAt(lineIndex)))
-                && currentLine.charAt(lineIndex) != '(' && currentLine.charAt(lineIndex) != ')')
-        {
-            Token temp = new Token(Character.toString(currentLine.charAt(lineIndex)), Token.Type.SPECIAL_SYMBOL);
-            lineIndex++;
-            if (lineIndex >= currentLine.length())
-            {
-                splitListIndex++;
-                if (splitListIndex >= splitList.length)
-                {
-                    return new Token("END OF INPUT", Token.Type.END_OF_INPUT);
-                }
-                currentLine = splitList[splitListIndex];
-                lineIndex = 0;
-
-            }
-            return temp;
-        } //If the current character is a letter
-        else if (Character.isAlphabetic(currentLine.charAt(lineIndex)))
-        {
-            //While the current character is a letter or a number
-            //build the token name and save it into the token string
-            //increment the line index
-            //if the current character is equal to a ) the stop working
-            //create and return a new Token
-            while (Character.isAlphabetic(currentLine.charAt(lineIndex)) || Character.isDigit(currentLine.charAt(lineIndex)))
-            {
-                token += Character.toString(currentLine.charAt(lineIndex));
-                lineIndex++;
-                if (lineIndex >= currentLine.length())
-                {
-                    splitListIndex++;
-                    if (splitListIndex >= splitList.length)
-                    {
-                        return new Token("END OF INPUT", Token.Type.END_OF_INPUT);
-                    }
-                    currentLine = splitList[splitListIndex];
-                    lineIndex = 0;
-
-                }
-                //A check for it a term is hyphenated
-                if (currentLine.charAt(lineIndex) == '-' && (Character.isAlphabetic(currentLine.charAt(lineIndex + 1)) || Character.isDigit(currentLine.charAt(lineIndex + 1))))
-                {
-                    token += Character.toString(currentLine.charAt(lineIndex));
-                    lineIndex++;
-                    if (lineIndex >= currentLine.length())
-                    {
-                        splitListIndex++;
-                        if (splitListIndex >= splitList.length)
-                        {
-                            return new Token("END OF INPUT", Token.Type.END_OF_INPUT);
-                        }
-                        currentLine = splitList[splitListIndex];
-                        lineIndex = 0;
-
-                    }
-                }
-                if(token.equals("member") && currentLine.charAt(lineIndex) == '?')
-                {
-                    token += "?";
-                    lineIndex++;
-
-                    if (lineIndex >= currentLine.length())
-                    {
-                        splitListIndex++;
-                        if (splitListIndex >= splitList.length)
-                        {
-                            return new Token("END OF INPUT", Token.Type.END_OF_INPUT);
-                        }
-                        currentLine = splitList[splitListIndex];
-                        lineIndex = 0;
-
-                    }
-
-                }
-                if(token.equals("null") && currentLine.charAt(lineIndex) == '?')
-                {
-                    token += "?";
-                    lineIndex++;
-
-                    if (lineIndex >= currentLine.length())
-                    {
-                        splitListIndex++;
-                        if (splitListIndex >= splitList.length)
-                        {
-                            return new Token("END OF INPUT", Token.Type.END_OF_INPUT);
-                        }
-                        currentLine = splitList[splitListIndex];
-                        lineIndex = 0;
-
-                    }
-
-                }
-                if(token.equals("set") && currentLine.charAt(lineIndex) == '!')
-                {
-                    token += "!";
-                    lineIndex++;
-
-                    if (lineIndex >= currentLine.length())
-                    {
-                        splitListIndex++;
-                        if (splitListIndex >= splitList.length)
-                        {
-                            return new Token("END OF INPUT", Token.Type.END_OF_INPUT);
-                        }
-                        currentLine = splitList[splitListIndex];
-                        lineIndex = 0;
-                    }
-                }
-
-                if (lineIndex >= currentLine.length() || currentLine.charAt(lineIndex) == ')')
-                {
-                    break;
-                }
-            }
-            if(is_Key_Word(token))
-            {
-                return new Token(token, Token.Type.KEYWORD);//Need to check for TYPE eventully
-            }
-            if(is_Procedure(token))
-            {
-                return new Token(token, Token.Type.PROCEDURE);//Need to check for TYPE eventully
-            }
-            return new Token(token, Token.Type.IDENTIFIER);//Need to check for TYPE eventully
-        } //If the current character is a digit
-        else if (Character.isDigit(currentLine.charAt(lineIndex)))
-        {
-            //While the current character is a digit
-            //build the token name and save it into the token string
-            //increment the line index
-            //if the current character is equal to a ) the stop working
-            //create and return a new Token
-            while (Character.isDigit(currentLine.charAt(lineIndex)))
-            {
-                token += Character.toString(currentLine.charAt(lineIndex));
-                lineIndex++;
-                if (lineIndex >= currentLine.length())
-                {
-                    splitListIndex++;
-                    if (splitListIndex >= splitList.length)
-                    {
-                        return new Token("END OF INPUT", Token.Type.END_OF_INPUT);
-                    }
-                    currentLine = splitList[splitListIndex];
-                    lineIndex = 0;
-
-                }
-                if (currentLine.charAt(lineIndex) == ')')
-                {
-                    break;
-                }
-            }
-            return new Token(token, Token.Type.NUMBER);////Need to check for TYPE eventully
-        }
-        return new Token("ERROR", Token.Type.ERROR);
     }
 
     private boolean is_Procedure(String key)
