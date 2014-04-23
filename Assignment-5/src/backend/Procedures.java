9435dbec363874a70c8c085a4df48ab1bd8ce901
Brandon Rossi <BrandonRossi@Brandons-MacBook-Pro-4.local>
2014-04-23 12:39:12 -0700

    Added Files to Backend
    Added a procedure class that will be used to evaluate list expressions,
    Also added a runtime stack class to maintain the runtime stack. and the
    runtime activation record class.

---
+++ Assignment-5/src/backend/Procedures.java
@@ -0,0 +1,81 @@
+/*
+ * I AM NOT SURE IF THESE METHODS SHOULD TAKE A SCHEME LIST OR NOT.
+ * I WOULD THINK THAT THEY SHOULD TAKE THE ROOT OF THE TREE THAT HOLDS THE LIST
+ * THAT NEEDS TO BE WORKED ON. TO COMPUTE THE SUM THE ROOT THAT REPRESENTS THE
+ * TREE STRUCTURE OF (+ 1 2 3 4) SHOULD BE BASSED TO THE METHOD. THEN THE METHOD
+ * SHOULD TRAVERSE THE TREE AND CALCULATE THE RESULT
+ */
+
+package backend;
+
+import intermediate.SchemeList;
+
+/**
+ * A class that is used to evaluate list expressions and procedures such as
+ * Addition, Subtraction, Multiplication, car, cdr, etc... 
+ * @author BrandonRossi
+ */
+public class Procedures
+{
+    public Procedures()
+    {
+
+    }
+
+    /*
+     * Calculate the sum
+     */
+    public int sum(SchemeList list)
+    {
+        return 0;
+    }
+
+    /*
+     * Calculate the difference
+     */
+    public int difference(SchemeList list)
+    {
+        return 0;
+    }
+
+    /*
+     * Calculate the quotient
+     */
+    public int quotient(SchemeList list)
+    {
+        return 0;
+    }
+
+    /*
+     * Calculate the product
+     */
+    public int product(SchemeList list)
+    {
+        return 0;
+    }
+
+    /*
+     * THIS SHOULD RETURN THE CAR OF THE LIST
+     */
+    public void car(SchemeList list)
+    {
+
+    }
+
+    /*
+     * THIS SHOULD RETURN THE CDR OF THE LIST
+     */
+    public void cdr(SchemeList list)
+    {
+
+    }
+
+    /*
+     *
+     */
+    public void list()
+    {
+
+    }
+}
+
